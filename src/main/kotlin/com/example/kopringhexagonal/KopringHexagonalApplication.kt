e60a694277efe9d24dd1b5caa99ee6e879e44b22
이하성 <tedsoftj1123@gmail.com>
2023-01-08 22:18:48 +0900

    프로젝트 생성

---
+++ src/main/kotlin/com/example/kopringhexagonal/KopringHexagonalApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kopringhexagonal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringHexagonalApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringHexagonalApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kopringhexagonal/KopringHexagonalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kopringhexagonal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringHexagonalApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
