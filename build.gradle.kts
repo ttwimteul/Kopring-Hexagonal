ae185c0d2111fe00e8a044b19cf2dff9183a8231
이하성 <tedsoftj1123@gmail.com>
2023-01-08 23:09:14 +0900

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,40 +2,42 @@ plugins {
     kotlin("jvm") version "1.7.22"
 }
 
-group = "com.example"
-version = "0.0.1-SNAPSHOT"
-
 subprojects {
     apply {
         plugin("org.jetbrains.kotlin.jvm")
         version = "1.6.21"
     }
 
+    repositories {
+        mavenCentral()
+    }
+
     dependencies {
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.3")
     }
 }
 
 
-tasks {
-    compileKotlin {
-        kotlinOptions {
-            freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "17"
+allprojects {
+    group = "com.example"
+    version = "0.0.1-SNAPSHOT"
+
+    tasks {
+        compileKotlin {
+            kotlinOptions {
+                freeCompilerArgs = listOf("-Xjsr305=strict")
+                jvmTarget = "17"
+            }
+        }
+
+        compileJava {
+            sourceCompatibility = JavaVersion.VERSION_17.majorVersion
         }
     }
 
-    compileJava {
-        sourceCompatibility = JavaVersion.VERSION_17.majorVersion
+    repositories {
+        mavenCentral()
     }
-
-    test {
-        useJUnitPlatform()
-    }
-}
-
-repositories {
-    mavenCentral()
 }
